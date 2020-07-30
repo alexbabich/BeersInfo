e5e5f9db5b3a839b58d675bb7a69a14bb1da1aaa
alex-babich <ababich.dev@gmail.com>
2020-07-30 16:35:27 +0300

    Initial Commit

---
+++ BeersInfo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeersInfo
+//
+//  Created by alex-babich on 30.07.2020.
+//  Copyright © 2020 alex-babich. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
